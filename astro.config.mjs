d9b3942757f702dd2235f08f34c7ec78dd423eeb
Marcus Otterström <marcus.otterstrom@caspeco.se>
2025-08-15 19:27:36 +0200

    Add plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightChangelogs, { makeChangelogsSidebarLinks } from "starlight-changelogs";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "My Docs",
+			plugins: [starlightChangelogs(), starlightLinksValidator()],
+			social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
+				},
+				{
+					label: "Changelogs",
+					items: [...makeChangelogsSidebarLinks([{ type: "recent", base: "changelog" }])],
 				},
 			],
 		}),
